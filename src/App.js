29a53dd7beac1e7fad581c763660b3de210b52d8
Mohit maurya <mauryamohit138@gmail.com>
2024-07-05 06:36:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import BarChart from "./Bar-chart/BarChart";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BarChart />
+      <div class="footer">
+        Created by <a href="https://github.com/Mohit138928">Mohit Maurya ❤</a>
+      </div>
+    </>
   );
 }
 
